3c78fa6b6fe8816e852998e78b0c59a4287f9f47
Miguel Ángel <migueabellan@gmail.com>
2021-12-24 17:38:15 +0100

    feat: update

---
+++ src/Classes/Coord2D.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Aoc\Classes;
+
+final class Coord2D
+{
+    public function __construct(
+        private int $x = 0,
+        private int $y = 0
+    ) {}
+
+    public function getX(): int
+    {
+        return $this->x;
+    }
+
+    public function setX(int $x): self
+    {
+        $this->x = $x;
+
+        return $this;
+    }
+
+    public function getY(): int
+    {
+        return $this->y;
+    }
+
+    public function setY(int $y): self
+    {
+        $this->y = $y;
+
+        return $this;
+    }
+
+    public function __toString(): string
+    {
+        return sprintf('(%d, %d)', $this->x, $this->y);
+    }
+}
---
+++ src/Command/PuzzleCommand.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace Aoc\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class PuzzleCommand extends Command
+{
+    protected static $defaultName = 'puzzle:exec';
+
+    protected function configure(): void
+    {
+        $currentYear = (new \DateTime())->format("Y");
+        $currentDay = (new \DateTime())->format("d");
+        $currentPuzzle = 1;
+
+        $this
+            ->setDescription('Outputs the solutions of a Puzzles for a given event')
+            ->addOption(
+                'year',
+                'y',
+                InputOption::VALUE_REQUIRED,
+                'the year of the event',
+                $currentYear
+            )
+            ->addOption(
+                'day',
+                'd',
+                InputOption::VALUE_REQUIRED,
+                'the day of the event',
+                $currentDay
+            )
+            ->addOption(
+                'puzzle',
+                'p',
+                InputOption::VALUE_REQUIRED,
+                'the puzzle of the day',
+                $currentPuzzle
+            );
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $year = $input->getOption('year');
+        $day = $input->getOption('day');
+        $puzzle = $input->getOption('puzzle');
+
+        try {
+            $class = sprintf('\\App\\Year%s\\Day%s\\Puzzle', $year, $day);
+            $runner = new $class();
+        } catch (\Error $e) {
+            $output->writeln(sprintf('<error>No class found for day %s of year %s</error>', $day, $year));
+            return Command::FAILURE;
+        }
+
+        switch ($puzzle) {
+            case 1:
+                $array = $runner->read();
+                $result = $runner->exec1($array);
+                $runner->write($result);
+                break;
+            case 2:
+                $array = $runner->read();
+                $result = $runner->exec2($array);
+                $runner->write($result);
+                break;
+            default:
+                $output->writeln(sprintf('<error>No method found for puzzle %s</error>', $puzzle));
+                return Command::FAILURE;
+        }
+
+        return Command::SUCCESS;
+    }
+}
